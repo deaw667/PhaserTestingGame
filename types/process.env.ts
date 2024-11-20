b5a5009ef89a68e19847509e84f9a3aa691ccbd6
deaw667 <57550597+deaw667@users.noreply.github.com>
2024-11-20 13:10:04 +0700

    Update process.env.ts

--- types/process.env.ts
+++ types/process.env.ts
@@ -1,11 +1,9 @@
 // define the Node "process" object. If it is defined (@types/node) by another module you installed,
 // then you can delete this file
 
-// Remove this file or modify it to check if process is already defined
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NODE_ENV: string;
-        }
+// types/process.env.ts
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: string;
     }
 }
